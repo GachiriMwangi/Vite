5d93c4356ad6b60b9f6708cca25bc66b0da8a29e
= <=>
2023-08-08 15:53:12 +0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <h1>Getting started with Vite.</h1> 
+    </div>
+
+    </>
+  )
+}
+
+export default App
