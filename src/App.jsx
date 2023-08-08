289eade66be9d495f7d6d03fecbe156e366a4000
= <=>
2023-08-08 16:02:15 +0300

    Description

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,15 @@ function App() {
     <>
     <div>
       <h1>Getting started with Vite.</h1> 
+   <p>Vite is a platform-agnostic frontend tool
+     for building web applications quickly and 
+     solves some common developer headaches.
+      Tim Davidson. In recent years, the frontend 
+      development ecosystem has seen an explosion of 
+      new tools and frameworks 
+    designed to improve the developer experience.</p>
+
+    <h4>How to get started.</h4>
     </div>
 
     </>
